60a70abae84d3ec30313c5f6e05390cdeacb8b2f
Usman <usmanzubair091@gmail.com>
2024-05-11 22:04:44 +0500

    Updated

--- src/auth.ts
+++ src/auth.ts
@@ -7,4 +7,21 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
+  callbacks: {
+    async session({token, session}) {
+      if(token) {
+        session.user.id = token.id
+        session.user.email = token.email as string
+        session.user.image = token.picture
+        session.user.name = token.name
+        session.user.username = token.username
+      }
+
+      return session;
+    }
+
+    async jwt({token, user}) {
+      
+    }
+  }
 })
